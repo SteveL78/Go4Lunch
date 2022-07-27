84ef6b66b1bf4929d223ea33d1348d0837a3791d
steveleroy <casaleroy@gmail.com>
2022-07-27 18:57:18 +0200

    add SignInActivity

---
+++ app/src/main/java/steve/leroy/go4lunch/authentication/SignInActivity.java
@@ -0,0 +1,16 @@
+package steve.leroy.go4lunch.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import steve.leroy.go4lunch.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_sign_in );
+    }
+}
