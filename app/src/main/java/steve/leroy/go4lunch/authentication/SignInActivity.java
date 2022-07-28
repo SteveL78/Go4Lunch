a3ecbc0fd7cb0efc37c4037569f7af2fa3c1c1f4
steveleroy <casaleroy@gmail.com>
2022-07-28 20:37:34 +0200

    add splash screen

--- app/src/main/java/steve/leroy/go4lunch/authentication/SignInActivity.java
+++ app/src/main/java/steve/leroy/go4lunch/authentication/SignInActivity.java
@@ -1,6 +1,9 @@
 package steve.leroy.go4lunch.authentication;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.core.splashscreen.SplashScreen;
 
 import steve.leroy.go4lunch.BaseActivity;
 import steve.leroy.go4lunch.databinding.ActivitySignInBinding;
@@ -14,8 +17,12 @@ public class SignInActivity extends BaseActivity<ActivitySignInBinding> {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen( this );
+
         super.onCreate( savedInstanceState );
 
+        //Hide StatusBar
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
 
     }
 }
