2e830e974c174739aaec4d10d07885f5a95579e1
steveleroy <casaleroy@gmail.com>
2023-05-07 20:14:53 +0200

    add user info (photo, name, email) in navigation drawer

--- app/src/main/java/steve/leroy/go4lunch/MainActivity.java
+++ app/src/main/java/steve/leroy/go4lunch/MainActivity.java
@@ -2,14 +2,24 @@ package steve.leroy.go4lunch;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import steve.leroy.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public ActivityMainBinding getViewBinding() {
@@ -24,6 +34,12 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
         this.configureDrawerLayout();
 
         this.configureBottomNavigation();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            updateDrawerHeader( user );
+        }
 
     }
 
@@ -41,6 +57,37 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     private void configureBottomNavigation() {
         NavController navController = Navigation.findNavController( this, R.id.nav_host_fragment );
         NavigationUI.setupWithNavController( binding.bottomNavigationMenu, navController );
+    }
+
+
+    private void updateDrawerHeader(FirebaseUser user) {
+        View headerView = binding.navigationView.getHeaderView( 0 );
+        ImageView userPhotoImageView = headerView.findViewById( R.id.header_navigation_user_photo );
+        TextView userNameTextView = headerView.findViewById( R.id.main_activity_header_user_name );
+        TextView userEmailTextView = headerView.findViewById( R.id.main_activity_header_user_email );
+
+        if (user != null && user.getPhotoUrl() != null) {
+            Glide.with(this)
+                    .load(user.getPhotoUrl())
+                    .apply( RequestOptions.circleCropTransform())
+                    .into(userPhotoImageView);
+        } else {
+            userPhotoImageView.setImageResource( R.drawable.anon_user );
+        }
+
+        if (user.getDisplayName() != null && !user.getDisplayName().isEmpty()) {
+            userNameTextView.setText( user.getDisplayName() );
+            userNameTextView.setVisibility( View.VISIBLE );
+        } else {
+            userNameTextView.setVisibility( View.GONE );
+        }
+
+        if (user.getEmail() != null && !user.getEmail().isEmpty()) {
+            userEmailTextView.setText( user.getEmail() );
+            userEmailTextView.setVisibility( View.VISIBLE );
+        } else {
+            userEmailTextView.setVisibility( View.GONE );
+        }
     }
 
 }
